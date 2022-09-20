5d036a48e8cd9aba7e1e1380d821d18f04220c7f
Juan Pablo Montoya <jpmontoya19@gmail.com>
2022-09-20 15:38:41 -0500

    Create Dockerfile for building the container

--- app/main.py
+++ app/main.py
@@ -1,3 +1,5 @@
+import os
+
 from flask import Flask
 
 app = Flask(__name__)
@@ -7,4 +9,5 @@ def hello():
     return "Hello world Flask"
 
 if __name__ == "__main__":
-    app.run(port=8080)
+    port = int(os.environ.get('PORT', 8080))
+    app.run(debug=True, host='0.0.0.0', port=port)
