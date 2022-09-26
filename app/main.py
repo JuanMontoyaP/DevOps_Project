4c1303ff810a653f1bb540254e1a1032b52dd3ff
JuanMontoyaP <jpmontoya19@gmail.com>
2022-09-26 00:23:41 -0500

    Add home page

--- app/main.py
+++ app/main.py
@@ -1,12 +1,45 @@
+from ensurepip import bootstrap
 import os
 
-from flask import Flask
+from flask import Flask 
+from flask import request, make_response, redirect
+from flask import render_template
+
+from flask_bootstrap import Bootstrap
 
 app = Flask(__name__)
 
+bootstrap = Bootstrap(app)
+
+tasks = ["Finish Flask App", "Set Database", "Jenkins pipeline"]
+
+@app.errorhandler(404)
+def not_found(error):
+    return render_template('404.html', error=error)
+
+@app.errorhandler(500)
+def server_error(error):
+    return render_template('500.html', error=error)
+
 @app.route('/')
+def index():
+    user_ip = request.remote_addr
+
+    response = make_response(redirect('/hello'))
+    response.set_cookie('user_ip', user_ip)
+
+    return response
+
+@app.route('/hello')
 def hello():
-    return "Hello world Flask"
+    user_ip = request.cookies.get('user_ip')
+
+    context = {
+        'user_ip': user_ip,
+        'tasks': tasks
+    }
+
+    return render_template('home.html', **context)
 
 if __name__ == "__main__":
     port = int(os.environ.get('PORT', 8000))
