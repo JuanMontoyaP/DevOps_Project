8e34064ac96296ae5e981c7eb4bb3aa52ce2bef1
JuanMontoyaP <jpmontoya19@gmail.com>
2022-09-27 16:30:52 -0500

    Create Movies page

--- app/app/api/views.py
+++ app/app/api/views.py
@@ -1,5 +1,16 @@
+from multiprocessing import context
+from flask import render_template
+
+from app.helpers.movies import get_popular_movies
+
 from . import api
 
 @api.route('/movies')
-def get_all_movies():
-    return "Movies"
+def movies():
+    popular_movies = get_popular_movies()
+    
+    context = {
+        'popular_movies': popular_movies
+    }
+
+    return render_template('movies.html', **context)
---
+++ app/app/helpers/movies.py
@@ -0,0 +1,21 @@
+import os
+from dotenv import load_dotenv
+import requests
+
+load_dotenv()
+
+base_url = "https://api.themoviedb.org/3/"
+access_api = f'?api_key={os.getenv("API_KEY")}'
+
+def get_movies_from_api():
+    api_url = f'{base_url}movie/popular{access_api}'
+    response = requests.get(api_url)
+    return response.json()["results"]
+
+def get_popular_movies():
+    
+    api_movies = get_movies_from_api()
+
+    movie_titles = [{"title": movie["title"], "vote_average": movie["vote_average"]} for movie in api_movies]
+
+    return movie_titles
