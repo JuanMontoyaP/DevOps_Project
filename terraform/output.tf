6118f711cb30149dd168e4f5d1e8fcab28c2aa0f
JuanMontoyaP <jpmontoya19@gmail.com>
2022-10-11 22:53:08 -0500

    Add output terraform

--- terraform/output.tf
+++ terraform/output.tf
@@ -1,3 +1,8 @@
 output "elb-dns-name" {
   value = aws_lb.aplication-lb.dns_name
 }
+
+output "instance_ip_addr" {
+  value       = aws_instance.cisco.*.public_ip
+  description = "The private IP address of the main server instance."
+}
